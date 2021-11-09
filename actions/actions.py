d621cacb3c344cc1b55c0069ee296b36bc0d4c39
Alessandro De Stefano <alexdestefano98@gmail.com>
2021-11-09 17:43:16 +0100

    deleting local data, strings for ros comm

--- actions/actions.py
+++ actions/actions.py
@@ -6,11 +6,6 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.types import DomainDict
-from nltk.stem import PorterStemmer
-
-stemmer = PorterStemmer()
-
-DATA = defaultdict(int)
 
 
 class ActionItem(Action):
@@ -46,36 +41,16 @@ class ActionItem(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         operation = tracker.get_slot("operation")
         item = tracker.get_slot("item")
-        # TODO: Use stem as id in the final database
         quantity = tracker.get_slot("CARDINAL")
-        print(f"received: {operation=} {item=} {quantity=}")
 
         item, quantity = self.sanitize_itemquantity(item, quantity)
 
-        item = stemmer.stem(item)
-
         if quantity == "":
             # when I say "remove item" I want to remove all of them
-            quantity = '1' if operation == "add" else DATA[item]
+            quantity = '1' if operation == "add" else "all"
 
-        dispatcher.utter_message(text=f"{operation=}")
-        if operation == "add":
-            dispatcher.utter_message(text=f"Adding: {quantity=}, {item=}.")
-            DATA[item] += int(quantity)
-        elif operation == "remove":
-            if DATA[item] == 0:
-                dispatcher.utter_message(text=f"No {item=} found in the shopping list. {quantity=}.")
-            else:
-                dispatcher.utter_message(text=f"Removing: {quantity=}, {item=}.")
-                new_quantity = DATA[item] - int(quantity)
-                if new_quantity <= 0:
-                    DATA.pop(item)
-                else:
-                    DATA[item] = new_quantity
-        else:
-            dispatcher.utter_message(text=f"Operation {operation} is invalid.")
+        dispatcher.utter_message(text=f"__{operation}__,{quantity},{item}")
 
-        print(f"set: {operation=} {item=} {quantity=}")
         return [SlotSet("item", None), SlotSet("operation", None), SlotSet("CARDINAL", None)]
 
 
@@ -84,12 +59,7 @@ class ActionShowItems(Action):
         return "action_show_items"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        if len(DATA) == 0:
-            dispatcher.utter_message(text='Your shopping list is empty')
-        else:
-            title = '# -------- SHOPPING LIST  -------- #\n'
-            rows = (f'{name} - {quantity}' for name, quantity in DATA.items())
-            dispatcher.utter_message(text=title + '\n'.join(rows))
+        dispatcher.utter_message(text="__show__")
         return []
 
 
@@ -108,8 +78,6 @@ class ValidateItemForm(FormValidationAction):
         quantity = tracker.get_slot("CARDINAL")
         quantity = "" if quantity is None else quantity
 
-        print(f"{item=} {quantity=}")
-
         nonnumeric_amount = len(list(filter(lambda x: not x.isnumeric(), quantity.split(
         )))) + len(list(filter(lambda x: not x.isnumeric(), item.split())))
 
@@ -119,38 +87,6 @@ class ValidateItemForm(FormValidationAction):
 
         dispatcher.utter_message(response="utter_default")
         return {"item": None}
-
-        # if item.replace(" ", '').isnumeric():
-        #     if quantity.replace(" ", '').isnumeric():
-        #         # item: quantity, quantity: quantity
-        #         dispatcher.utter_message(response="utter_default")
-        #         return {"item": None}
-        #     else:
-        #         # item: quantity, quantity: item
-        #         return {"item": slot_value}
-
-        # quantity=quantity+item, item: ?
-        # numbers = list(filter(lambda x: x.isnumeric(), quantity.split()))
-        # if 0 < len(numbers) < len(slot_value.split()):
-        #     return {"item": slot_value}
-
-        # if quantity is not None and quantity in slot_value:
-        #     slot_value = slot_value.replace(quantity, '').strip()
-        # return {"item": None if len(slot_value) == 0 else slot_value}
-
-    # def validate_CARDINAL(
-    #     self,
-    #     slot_value: Any,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: DomainDict,
-    # ) -> Dict[Text, Any]:
-    #     quantity = "1"
-    #     if slot_value is not None:
-    #         for word in slot_value.split(" "):
-    #             if word.isnumeric():
-    #                 quantity = word
-    #     return {"CARDINAL": quantity}
 
     def validate_operation(
         self,
