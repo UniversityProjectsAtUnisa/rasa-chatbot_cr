b7654e16cd4fc6b2459753030b8667dd951dd3d5
Marco Della Rocca <marco741147@gmail.com>
2021-11-10 03:23:25 +0100

    Modularized test generator

--- scripts/test_generator.py
+++ scripts/test_generator.py
@@ -6,12 +6,6 @@ from functools import reduce
 from pathlib import Path
 from num2words import num2words
 
-random.seed(0)
-
-item_lut = "data/lookups/item.yml"
-
-test_file = "tests/test_stories_python.yml"
-Path(test_file).parent.mkdir(parents=True, exist_ok=True)
 
 
 def map_idx_to_synonym(idx, n_synonyms: dict):
@@ -20,47 +14,71 @@ def map_idx_to_synonym(idx, n_synonyms: dict):
             return k, v[idx]
         idx -= len(v)
 
+def get_obj_from_yml(filename):
+    with open(filename) as yamlfile:
+        return yaml.load(yamlfile, Loader=Loader)
 
-with open(item_lut) as yamlfile:
-    obj = yaml.load(yamlfile, Loader=Loader)
+def dump_obj_on_yml(filename, data):
+    with open(filename, "w") as testfile:
+        yaml.dump(data, testfile)
+
+def get_items_from_yml(filename):
+    obj = get_obj_from_yml(filename)
     raw_items: str = obj['nlu'][0]['examples']
     items = [item[2:] for item in raw_items.split('\n')]
+    return items
 
-tests = {"stories": []}
 
-synonyms = {"add": ["add", "put", "buy", "insert"],
-            "remove": ["remove", "delete", "destroy"]}
 
-n_synonyms = reduce(lambda acc, n: acc+len(synonyms[n]), synonyms, 0)
-r = random.randint(0, n_synonyms - 1)
 
-for (idx, item) in enumerate(items):
-    ret = map_idx_to_synonym((idx+r) % n_synonyms, synonyms)
-    synonym, operation = ret
+def main():
+    random.seed(0)
 
-    r_number = (idx+r) % 3
-    number = ""
-    if r_number == 0:
-        number = f"[{idx}](CARDINAL)"
-    elif r_number == 1:
-        number = f"[{num2words(idx)}](CARDINAL)"
-    single_test = {
-        "story": f"LUT test {operation}({synonym}) {item}",
-        "steps": [
-            {"action": "action_listen"},
-            {"user": f'[{operation}]' + r'{"entity": "operation", "value": "'+synonym+'"}',
-             "intent": "operation_on_item"},
-            {"action": "item_form"},
-            {"active_loop": "item_form"},
-            {"action": "action_listen"},
-            {"user": f"{number} [{item}](item)",
-             "intent": "operation_on_item"},
-            {"action": "item_form"},
-            {"active_loop": None},
-            {"action": "action_item"},
-        ]
-    }
-    tests["stories"].append(single_test)
+    item_lut = "data/lookups/item.yml"
 
-with open(test_file, "w") as testfile:
-    yaml.dump(tests, testfile)
+    test_file = "tests/test_stories_python.yml"
+    Path(test_file).parent.mkdir(parents=True, exist_ok=True)
+
+    items = get_items_from_yml(item_lut)
+
+    tests = {"stories": []}
+
+    synonyms = {"add": ["add", "put", "buy", "insert"],
+                "remove": ["remove", "delete", "destroy"]}
+
+    n_synonyms = reduce(lambda acc, n: acc+len(synonyms[n]), synonyms, 0)
+    r = random.randint(0, n_synonyms - 1)
+
+    for (idx, item) in enumerate(items):
+        ret = map_idx_to_synonym((idx+r) % n_synonyms, synonyms)
+        synonym, operation = ret
+
+        r_number = (idx+r) % 2
+        number = ""
+        if r_number == 0:
+            number = f"[{idx}](CARDINAL)"
+
+        single_test = {
+            "story": f"LUT test {operation}({synonym}) {item}",
+            "steps": [
+                {"action": "action_listen"},
+                {"user": f'[{operation}]' + r'{"entity": "operation", "value": "'+synonym+'"}',
+                "intent": "operation_on_item"},
+                {"action": "item_form"},
+                {"active_loop": "item_form"},
+                {"action": "action_listen"},
+                {"user": f"{number} [{item}](item)",
+                "intent": "operation_on_item"},
+                {"action": "item_form"},
+                {"active_loop": None},
+                {"action": "action_item"},
+            ]
+        }
+        tests["stories"].append(single_test)
+
+    dump_obj_on_yml(test_file, tests)
+
+    
+
+if __name__ == "__main__":
+    main()
