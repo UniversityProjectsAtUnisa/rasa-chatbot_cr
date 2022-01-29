b46405db08f50a2f19250f5bf2711bce6aeede55
EarendilTiwele <alexdestefano98@gmail.com>
2022-01-29 20:03:26 +0100

    user features, not complete

--- scripts/generate_operation_examples.py
+++ scripts/generate_operation_examples.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from test_generator import get_items_from_yml
+from test_generator import get_examples_from_yml
 import random
 import argparse
 from pathlib import Path
@@ -8,9 +8,10 @@ from pathlib import Path
 
 PROJECT_PATH = Path(__file__).absolute().parent.parent
 
+
 def item_generator():
     item_file = PROJECT_PATH.joinpath('data/lookups/item.yml')
-    items = get_items_from_yml(item_file)
+    items = get_examples_from_yml(item_file)
     while True:
         yield random.choice(items)
 
@@ -40,6 +41,13 @@ def CARDINAL_generator():
         yield str(clamped)
 
 
+def user_generator():
+    user_file = PROJECT_PATH.joinpath('data/lookups/user.yml')
+    users = get_examples_from_yml(user_file)
+    while True:
+        yield random.choice(users)
+
+
 def main(seed: int, template_amt: int, basic_only: bool):
     random.seed(seed)
     AMOUNT_PER_TEMPLATE = template_amt
@@ -57,37 +65,44 @@ def main(seed: int, template_amt: int, basic_only: bool):
 
     if not basic_only:
         templates += ["(operation) (CARDINAL) (item) from the shopping list",
-                    "(operation) (CARDINAL) (item) to the shopping list",
-                    "(operation) (item) from the shopping list",
-                    "(operation) (item) to the shopping list",
-                    "can I (operation) some (item)",
-                    "i want to (operation)",
-                    "i want to (operation) (CARDINAL) (item)",
-                    "i want to (operation) (CARDINAL) (item) from my list",
-                    "i want to (operation) (CARDINAL) (item) from my shopping list",
-                    "i want to (operation) (CARDINAL) (item) to my list",
-                    "i want to (operation) (CARDINAL) (item) to my shopping list",
-                    "i want to (operation) (item)",
-                    "i want to (operation) some (item)",
-                    "i would like to (operation) (CARDINAL) (item)",
-                    "i would like to (operation) (item)",
-                    "i would like to (operation) a (item)",
-                    "i'd like to (operation) (CARDINAL) (item)",
-                    "i'd like to (operation) (CARDINAL) (item) from my list",
-                    "i'd like to (operation) (CARDINAL) (item) from my shopping list",
-                    "i'd like to (operation) (CARDINAL) (item) to my list",
-                    "i'd like to (operation) (CARDINAL) (item) to my shopping list",
-                    "i'd like to (operation) (item)",
-                    "i'd like to (operation) (item) from my list",
-                    "i'd like to (operation) (item) from my shopping list",
-                    "i'd like to (operation) (item) to my list",
-                    "i'd like to (operation) (item) to my shopping list",
-                    ]
+                      "(operation) (CARDINAL) (item) to the shopping list",
+                      "(operation) (item) from the shopping list",
+                      "(operation) (item) to the shopping list",
+                      "can i (operation) some (item)",
+                      "i want to (operation)",
+                      "i want to (operation) (CARDINAL) (item)",
+                      "i want to (operation) (CARDINAL) (item) from my list",
+                      "i want to (operation) (CARDINAL) (item) from my shopping list",
+                      "i want to (operation) (CARDINAL) (item) to my list",
+                      "i want to (operation) (CARDINAL) (item) to my shopping list",
+                      "i want to (operation) (item)",
+                      "i want to (operation) some (item)",
+                      "i would like to (operation) (CARDINAL) (item)",
+                      "i would like to (operation) (item)",
+                      "i would like to (operation) a (item)",
+                      "i'd like to (operation) (CARDINAL) (item)",
+                      "i'd like to (operation) (CARDINAL) (item) from my list",
+                      "i'd like to (operation) (CARDINAL) (item) from my shopping list",
+                      "i'd like to (operation) (CARDINAL) (item) to my list",
+                      "i'd like to (operation) (CARDINAL) (item) to my shopping list",
+                      "i'd like to (operation) (item)",
+                      "i'd like to (operation) (item) from my list",
+                      "i'd like to (operation) (item) from my shopping list",
+                      "i'd like to (operation) (item) to my list",
+                      "i'd like to (operation) (item) to my shopping list",
+                      "i am (user)",
+                      "i'm (user)",
+                      "it is me (user)",
+                      "it's me (user)",
+                      "my name is (user)",
+                      "my name's (user)",
+                      ]
 
     entity_mapper = {
         "(item)": item_generator(),
         "(operation)": operation_generator(),
-        "(CARDINAL)": CARDINAL_generator()
+        "(CARDINAL)": CARDINAL_generator(),
+        "(user)": user_generator()
     }
 
     examples = set()
@@ -112,8 +127,10 @@ def main(seed: int, template_amt: int, basic_only: bool):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", type=int, default=0, help="Seed identifier for the random generation")
-    parser.add_argument("--template-amt", type=int, default=100, help="How many phrases per template")
+    parser.add_argument("--seed", type=int, default=0,
+                        help="Seed identifier for the random generation")
+    parser.add_argument("--template-amt", type=int,
+                        default=100, help="How many phrases per template")
 
     parser.add_argument('--all', dest='basic_only', action='store_false')
     parser.add_argument('--basic-only', dest='basic_only', action='store_true')
